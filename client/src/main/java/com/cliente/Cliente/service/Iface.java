b99b8ef54ac8648ffb045cbc039b4363a5dd456a
jhonsanchezgomez <48131958+jhonsanchezgomez@users.noreply.github.com>
2019-03-18 11:01:42 -0500

    Update Iface.java

--- client/src/main/java/com/cliente/Cliente/service/Iface.java
+++ client/src/main/java/com/cliente/Cliente/service/Iface.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "http://apiZuul")
+@FeignClient(name = "apiZuul")
 public interface Iface {
 	
 	@RequestMapping(method = RequestMethod.GET, value = "mascota/consulta/{id}")
